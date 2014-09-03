5805728227dba4e4e4345bb08303a64a7393f017
plade1 <sailpra@gmail.com>
2014-09-03 06:06:20 -0700

    added methods to extract eyebrow features

--- frame.h
+++ frame.h
@@ -1,16 +1,15 @@
-#ifndef FRAME_H
-#define FRAME_H
-
 #include <math.h>
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <iostream>
-#include <cmath> 
+#include <cmath>
+#include <opencv2/core/core.hpp>
+
 using namespace std;
 
-struct point 
+struct point
 {
 	float x;
 	float y;
@@ -18,35 +17,35 @@ struct point
 
 class frame
 {
-	
+
 	private:
 		void matrix_mult(float angle);
-		void matrix_mult(float angle, std::vector<point> &inp_mat);
-		void create_brow_landmarks();
-		void add_translation(point trans, std::vector<point> &matrix);
-		void extract_left_eyebrow_feat(point neutral_ctrd);
-		void extract_right_eyebrow_feat(point neutral_ctrd);
+		std::vector<point> matrix_mult(float angle, std::vector<point> inp_mat);
+		std::vector<point> add_translation(point trans, std::vector<point> matrix);
 		point *calc_centroid(std::vector<point> neutral_marks);
-		std::vector<float> get_feats(std::vector<point> const &lndmarks);
-		static float get_probability(float feat, float mean, float std, float precision);
-		static std::vector<float> calc_eye_feat_ratio(std::vector<float> peek_feat, std::vector<float> neutral_feat);
+		std::vector<float> get_dist(std::vector<point> lndmarks);
+		static float get_probability(float feat, float mean, float std);
 
-	public: 
-		std::vector<point> r_eyebrow_landmks; // Holds the INDICIES of the right eyebrow landmarks
-		std::vector<point> l_eyebrow_landmks; // Holds the INDICIES of the left eyebrow landmarks
+	public:
+		std::vector<point> r_eye_region_landmks; 
+		std::vector<point> l_eye_region_landmks;
 		std::vector<point> landmarks;
-		std::vector<float> left_feats;
-		std::vector<float> right_feats;
+		std::vector<float> l_eyebrow_feats;
+		std::vector<float> r_eyebrow_feats;
+		std::vector<float> l_eye_feats;
+		std::vector<float> r_eye_feats;
 		point l_centroid;
 		point r_centroid;
-		
+
 		frame();
 		frame(std::vector<int> input_file, int start_row=0);
+		frame(cv::Mat cvLandmarks);
 		void rotate();
 		void calc_centroids();
-		void extract_eyebrow_feat(frame *neutral_frame);
+		std::vector<float> extract_eyebrow_feat(point neutral_ctrd, std::vector<point> lndmarks);
+		std::vector<float> extract_eye_feat(point neutral_ctrd, std::vector<point> lndmarks);
+		void extract_eye_region_landmarks();
 		static std::vector<float> calc_probs(std::vector<float> model_vals, std::vector<float> feat_vals);
-		static std::vector<float> eye_feat_ratio(frame *peak_frame, frame *neutral_frame);
+		static std::vector<float> eyebrow_feat_ratio(std::vector<float> peek_feat, std::vector<float> neutral_feat);
+		static std::vector<float> eye_feat_ratio(std::vector<float> peek_feat, std::vector<float> neutral_feat);
 	};
-
-#endif
